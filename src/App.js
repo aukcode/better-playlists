f6a5422fb939fca5f6190f645f6d804c03a1176c
auk <oyvind.auk@gmail.com>
2018-04-24 15:04:34 +0200

    Added MOAR data, finnished video 10

--- src/App.js
+++ src/App.js
@@ -74,11 +74,16 @@ class HoursCounter extends Component{
 
 class Playlist extends Component{
   render(){
+    let playlist = this.props.playlist
     return(
       <div style={{...defaultStyle, display: "inline-block", width: "25%"}}>
         <img/>
-        <h3>Playlist Name</h3>
-        <ul><li>Song 1</li><li>Song 2</li><li>Song 3</li></ul>
+        <h3>{playlist.name}</h3>
+        <ul>
+          {playlist.songs.map(song =>
+            <li>{song.name}</li>
+          )}
+        </ul>
       </div>
     );
   }
@@ -124,13 +129,16 @@ class App extends Component {
             <PlaylistCounter playlists={this.state.serverData.user.playlists}/>
             <HoursCounter playlists={this.state.serverData.user.playlists}/>
 
-        <Filter/>
-        <Playlist/>
-        <Playlist/>
-        <Playlist/>
-        <Playlist/>
-      </div>: <h1>Loading...</h1>
-      }
+            <Filter/>
+
+            {this.state.serverData.user.playlists.map(playlist =>
+              <Playlist playlist={playlist}/>
+            )}
+
+
+
+          </div>: <h1>Loading...</h1>
+        }
       </div>
     );
   }
