93d8956e78c6813f69c36602346ed0c9c34edefe
auk <oyvind.auk@gmail.com>
2018-04-24 20:22:33 +0200

    Fixing prodoction sign in button

--- src/App.js
+++ src/App.js
@@ -182,7 +182,12 @@ class App extends Component {
               <Playlist playlist={playlist}/>
             )}
 
-          </div> : <button onClick={() => window.location = 'http://localhost:8888/login' }
+          </div> : <button onClick={() => {
+            window.location = window.location.includes('localhost')
+            ? 'http://localhost:8888/login'
+            : 'https://aukplaylists.herokuapp.com/login'
+            }
+          }
             style={{padding: '20px', 'font-size': '50px',
              'margin-top': '20px'}}>Sign in with Spotify</button>
           }
