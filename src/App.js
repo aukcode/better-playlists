ed90262549b20b1aa6515bc300f419c304466e7d
auk <oyvind.auk@gmail.com>
2018-04-24 20:09:51 +0200

    adding spotify API and implementing actual server data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 //import logo from './logo.svg';
 import './App.css';
+import queryString from 'query-string';
 
 let textColor = '#fff';
 let defaultStyle = {
@@ -54,6 +55,8 @@ let fakeServerData = {
   }
 }
 
+
+
 class PlaylistCounter extends Component{
   render(){
     return(
@@ -85,7 +88,7 @@ class Playlist extends Component{
     let playlist = this.props.playlist
     return(
       <div style={{...defaultStyle, display: "inline-block", width: "25%"}}>
-        <img/>
+        <img src={playlist.imageUrl} style={{width: '120px'}}/>
         <h3>{playlist.name}</h3>
         <ul>
           {playlist.songs.map(song =>
@@ -119,13 +122,35 @@ class App extends Component {
     }
   }
   componentDidMount(){
-    setTimeout(() => {
-        this.setState({serverData: fakeServerData})
-    }, 1000)
+    let parsed = queryString.parse(window.location.search)
+    let accessToken = parsed.access_token
+
+    fetch('https://api.spotify.com/v1/me', {
+      headers: {'Authorization': 'Bearer ' + accessToken}
+    }).then(response => response.json())
+    .then(data => this.setState({
+      user: {
+        name: data.id
+      }
+    }))
+
+    fetch('https://api.spotify.com/v1/me/playlists', {
+      headers: {'Authorization': 'Bearer ' + accessToken}
+    }).then(response => response.json())
+    .then(data => this.setState({
+      playlists: data.items.map(item => ({
+        name: item.name,
+        imageUrl: item.images[0].url,
+        songs: []
+      }))
+    }))
   }
 
   render() {
-    let playlistToRender = this.state.serverData.user ? this.state.serverData.user.playlists
+    let playlistToRender =
+      this.state.user &&
+      this.state.playlists ?
+      this.state.playlists
     .filter(playlist =>
       playlist.name.toLowerCase().includes(
         this.state.filterString.toLowerCase())
@@ -133,7 +158,7 @@ class App extends Component {
 
     return (
       <div className="App">
-        {this.state.serverData.user ?
+        {this.state.user ?
           //This checks if we have a user and if so returns the stuff
           //after. Only workes on one tag, hence the following div
           // ? = turnary operator
@@ -142,8 +167,9 @@ class App extends Component {
           <div>
 
             <h1 style={{...defaultStyle, 'font-size': '54px'}}>
-              {this.state.serverData.user.name}
+              {this.state.user.name}
               's Playlists</h1>
+
 
             <PlaylistCounter playlists={playlistToRender}/>
             <HoursCounter playlists={playlistToRender}/>
@@ -154,8 +180,10 @@ class App extends Component {
               <Playlist playlist={playlist}/>
             )}
 
-          </div>: <h1>Loading...</h1>
-        }
+          </div> : <button onClick={() => window.location = 'http://localhost:8888/login' }
+            style={{padding: '20px', 'font-size': '50px',
+             'margin-top': '20px'}}>Sign in with Spotify</button>
+          }
       </div>
     );
   }
